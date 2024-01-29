4dc0fe036ea4cf934acdf9e7bb60ff0243865cd7
Nazmul1582 <nazmulhasan1582@gmail.com>
2024-01-29 22:12:42 +0600

    created the modal

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,17 +1,61 @@
-const Product = ({item}) => {
+import { useState } from "react";
+import { RxCross2 } from "react-icons/rx";
+
+
+const Product = ({ item }) => {
+  const [openModal, setOpenModal] = useState(false);
   return (
-    <div className="p-5 rounded-2xl bg-white shadow-lg cursor-pointer">
-      <img
-        src="https://avatars.mds.yandex.net/get-eda/3569651/b272d3b5f10746dfb125abca442a78be/400x400nocrop"
-        alt="product-photo"
-      />
-      <p className="text-3xl font-semibold">{item.price}</p>
-      <p className="mt-1 mb-3">{item.name}</p>
-      <p className="text-[#9E9B98] text-md my-3">{item.quantity}</p>
-      <button className="bg-[#F5F4F2] hover:bg-[#dddcda] duration-200 px-2 py-1 rounded-2xl font-semibold w-full text-2xl">
-        +
-      </button>
-    </div>
+    <>
+      <div        
+        className="p-5 rounded-2xl bg-white shadow-lg"
+      >
+        <div className="cursor-pointer" onClick={() => setOpenModal(true)}>
+
+        <img
+          src="https://avatars.mds.yandex.net/get-eda/3569651/b272d3b5f10746dfb125abca442a78be/400x400nocrop"
+          alt="product-photo"
+          />
+        <p className="text-3xl font-semibold">{item.price}</p>
+        <p className="mt-1 mb-3">{item.name}</p>
+        <p className="text-[#9E9B98] text-md my-3">{item.quantity}</p>
+          </div>
+        <button className="bg-[#F5F4F2] hover:bg-[#dddcda] duration-200 px-2 py-1 rounded-2xl font-semibold w-full text-2xl">
+          +
+        </button>
+      </div>
+      {openModal && (
+        <div className="fixed z-10 top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50">
+          <div className="bg-white p-8 rounded-lg shadow-lg w-3/4 h-3/4 relative">
+            <div className="grid grid-cols-2 gap-20 mx-20 items-center">
+              <img
+                src="https://avatars.mds.yandex.net/get-eda/3569651/b272d3b5f10746dfb125abca442a78be/400x400nocrop"
+                alt="product-photo"
+              />
+              <div className="p-5">
+                <p className="mt-1 text-2xl font-bold mb-3">{item.name}</p>
+                <p className="text-[#9E9B98] text-md my-3">{item.quantity}</p>
+                <div className="flex items-center gap-5">
+                  <p className="text-3xl font-bold">{item.price}</p>
+                  <button className="bg-amber-400 duration-200 px-5 py-2 rounded-2xl font-semibold text-2xl w-fit">
+                    Add
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <button
+              className="text-2xl absolute top-5 right-5"
+              onClick={(event) => {
+                event.stopPropagation();
+                setOpenModal(false);
+              }}
+            >
+              <RxCross2 />
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
